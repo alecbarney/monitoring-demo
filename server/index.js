5f7b269d499e8662de4ac699f518a846415a0863
Alec Barney <alecbarney34@gmail.com>
2022-02-24 12:12:11 -0700

    rollbar

--- server/index.js
+++ server/index.js
@@ -7,6 +7,16 @@ app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '40df665bf6ed44e39adfa2208d337f7f',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const port = process.env.PORT || 4545
 
 app.listen(port, ()=>{
